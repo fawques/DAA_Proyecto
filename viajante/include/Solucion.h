ff365b2707e35fea0508d2b41eb71d829a2e91bd
Víctor Guzmán <fawques7@hotmail.com>
2011-12-15 17:34:11 +0100

    Añadida clase solución, código duplicado, funcionando

---
+++ viajante/include/Solucion.h
@@ -0,0 +1,42 @@
+/* 
+ * File:   Solucion.h
+ * Author: victor
+ *
+ * Created on 15 de diciembre de 2011, 15:43
+ */
+
+#ifndef SOLUCION_H
+#define	SOLUCION_H
+
+#include <vector>
+#include "Vertice.h"
+#include "Arista.h"
+
+using namespace std;
+
+class Solucion {
+public:
+    Solucion(int num);
+    Solucion(const Solucion& orig);
+    
+    virtual ~Solucion();
+    
+    int getN(){
+        return n;
+    }
+    void setN(int num){
+        n = num;
+    }
+    
+    void setPeso(Vertice vertices[]);
+    
+    bool operator<(const Solucion& sol);
+    
+    vector <int> solucion;
+private:
+    int peso;
+    int n;
+};
+
+#endif	/* SOLUCION_H */
+
---
+++ viajante/lib/Solucion.cpp
@@ -0,0 +1,88 @@
+/* 
+ * File:   Solucion.cpp
+ * Author: victor
+ * 
+ * Created on 15 de diciembre de 2011, 15:43
+ */
+
+#include "Solucion.h"
+
+Solucion::Solucion(int num) {
+    n = num;
+    solucion = vector<int>(num+1,-1);
+    peso = 0;
+}
+
+Solucion::Solucion(const Solucion& orig) {
+    solucion = orig.solucion;
+    n = orig.n;
+    peso = orig.peso;
+}
+
+Solucion::~Solucion() {
+    solucion.clear();
+    n = 0;
+    peso = 0;
+}
+
+// Este operator< está implementado solo para poder insertar las soluciones en una PriorityQueue.
+// Devuelve lo que debería devolver un operator>
+bool Solucion::operator <(const Solucion& sol)
+{
+    if(peso < sol.peso)
+        return false;
+    else
+        return true;
+}
+
+int getVertice_aux(int id, Vertice vertices[], int cantidad)
+{
+    bool encontrado = false;
+    int i;
+    for(i = 0; i < cantidad && !encontrado; i++)
+    {
+        if(vertices[i].getID() == id)
+        {
+            encontrado = true;
+            break;
+        }
+    }
+    
+    if(!encontrado)
+        i = -1;
+    
+    return i;
+}
+
+void Solucion::setPeso(Vertice vertices[])
+{
+    int peso_total = 0;
+    // sacamos el elemento 0 de la solución.
+    
+
+    for(int j = 0; j < n; j++)
+    {
+        if(solucion[j] == -1 || solucion[j+1] == -1)
+            break;
+        
+        int pos = getVertice_aux(solucion[j],vertices,n);
+        if(pos != -1)
+        {
+            Arista* aristas = vertices[pos].getAristas();
+
+            for(int i = 0; i < vertices[pos].getGrado(); i++)
+            {
+                // si hay una arista entre los nodos de la solucion
+                if(aristas[i].getIdFinal() == solucion[j+1] && aristas[i].getIdInicio() == solucion[j])
+                {
+                    peso_total += aristas[i].getPeso();
+                }
+            }
+        }
+        else
+            cerr<<"ERROR!!!!!!"<<endl;
+    }
+    
+    peso = peso_total;
+}
+
